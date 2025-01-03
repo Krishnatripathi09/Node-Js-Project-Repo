2c84fe649fa6ec72215af684f64ff959705a5c64
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-01-03 10:33:02 +0530

    feat: created an Express server listening on port 3000

--- OneDrive/Documents/NODE-PROJECT-DEV-TINDER/src/app.js
+++ OneDrive/Documents/NODE-PROJECT-DEV-TINDER/src/app.js
@@ -1,1 +1,8 @@
-console.log("starting the project");
+const express = require("express"); //we have installed express in our project and it is installed in Node_module so we are importing it from there
+const app = express();
+
+app.listen(3000);
+
+app.use((req, res) => {
+  res.send("Hello World");
+});
