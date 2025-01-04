886f298b23c9fae5824c7df5f87c7aad3cde9574
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-01-04 19:41:16 +0530

    feat: created userSchema and User model for managing user data

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,6 @@ const { mongoose } = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },
-  Age: { type: Number },
   email: { type: String },
   password: { type: String },
   gender: { type: String },
