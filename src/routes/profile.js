17d576ba93e75a4905d1ac45bfeb9dfbd8af168d
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-01-08 12:29:05 +0530

    feat: Added API for updating user profile fields
    - Created an API to update user profile fields, including firstName, lastName, email, and gender.
    - Added validation to ensure proper data is provided for updates.
    - Utilized efficient database operations to modify user data securely.

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,12 +1,29 @@
 const { userAuth } = require("../middlewares/auth");
+const { validateProfileEditData } = require("../utils/validation");
 const express = require("express");
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateProfileEditData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    await loggedInUser.save();
+
+    res.send(`${loggedInUser.firstName},Your Profile Was Updated Successfully`);
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
   }
