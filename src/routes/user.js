e34247972088d94d01967829c2f73bdfe46d81f5
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-01-10 17:25:21 +0530

    feat(api): added API to fetch accepted connections
    Created GET /user/connections to retrieve all accepted connections for the logged-in user.
    Added authentication and error handling for secure and reliable data retrieval.

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,11 +9,32 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", ["firstName", "lastName"]);
+    }).populate("fromUserId", ["firstName", "lastName", "gender"]);
 
     res.json({
       message: "Data Fetched SuccessFully",
       data: connectionRequests,
+    });
+  } catch (err) {
+    req.statusCode(400).send("ERROR" + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", ["firstName", "lastName"]);
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+
+    res.json({
+      message: "Connections fetched Successfully",
+      data,
     });
   } catch (err) {
     req.statusCode(400).send("ERROR" + err.message);
