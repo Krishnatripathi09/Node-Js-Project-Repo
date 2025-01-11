38411277a866ef61392f5ade20a7a397653b822c
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-01-11 15:05:05 +0530

    feat(api): update feed API to display users eligible for connection requests
    - Enhanced the feed API to include a list of users to whom the logged-in user can send connection requests.
    - Added logic to filter out users with existing requests or connections.
    - Improved user experience by showing potential connections.

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const USER_SAFE_DATA = "firstName lastName gender";
-
+const User = require("../models/user");
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -52,8 +52,30 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    console.log(hideUsersFromFeed);
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        {
+          _id: { $ne: loggedInUser._id },
+        },
+      ],
+    });
+
+    res.send(users);
   } catch (err) {
-    req.status(400).res.json({ message: err.message });
+    res.status(400).json({ message: err.message });
   }
 });
 
